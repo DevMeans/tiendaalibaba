44090560a0d324a662f03b08837f9561c6563365
tavata <tavata.canales@gmail.com>
2024-11-01 12:36:27 -0500

    deploy para subir

--- src/app/(tienda)/page.tsx
+++ src/app/(tienda)/page.tsx
@@ -5,8 +5,6 @@ export default async function Home() {
   const products = await getPublicProduct()
   return (
     <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-5 max-w-[1200px] m-auto'>
-
-
       {
         products.map((item) => (
           <CardProductComponent key={item.id} product={item}></CardProductComponent>
--- src/app/(tienda)/product/[slug]/page.tsx
+++ src/app/(tienda)/product/[slug]/page.tsx
@@ -2,10 +2,25 @@
 import 'swiper/css';
 import AddCartPage from './AddCart';
 import { getProductSlug } from '@/actions/product/get-product-slug';
+import Head from 'next/head';
+import { Metadata } from 'next';
 interface Props {
   params: {
     slug: string
   }
+}
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = params;
+  const product = await getProductSlug(slug);
+
+  return {
+    title: product ? product.name : 'Producto no encontrado',
+    description: product ? product.description : 'No hay descripción disponible',
+    openGraph: {
+      title: product ? product.name : 'Producto no encontrado',
+      description: product ? product.description : 'No hay descripción disponible',
+    },
+  };
 }
 export default async function ProductSlug({ params }: Props) {
   const { slug } = params
@@ -13,11 +28,11 @@ export default async function ProductSlug({ params }: Props) {
   const product = await getProductSlug(slug)
   return (
     <>
+
       <main className="max-w-[1200px] p-5 m-auto grid grid-cols-1 md:grid-cols-2 mt-5 gap-10 ">
         {
           (product == null) ? 'No hay producto' : <AddCartPage product={product}></AddCartPage>
         }
-
       </main>
     </>
   );
